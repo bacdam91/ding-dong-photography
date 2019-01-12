f93c4cf3327f9ce1f7b3b8495ba6e895f3e63ff2
bacdam91 <bacdam91@hotmail.com>
2019-01-13 03:57:06 +1100

    Better looking splash screen

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function()
 {
-    $(".company-name-container h2, .company-name-container h4").click(function()
+    $(".company-name-container h1, .company-name-container h4").click(function()
     {
         window.location.href = "home.html";
     })
