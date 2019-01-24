df3a0bd2f85bbd0d8c1c53e01bb738648c05bc58
bacdam91 <bacdam91@hotmail.com>
2019-01-25 01:03:42 +1100

    implemented gallery

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,5 +1,12 @@
 $(document).ready(function()
 {  
+    var galleryItemsPos = getPosOfGalleryItems();
+    var widthArray = getGalleryItemsWidth(galleryItemsPos);
+
+    console.log(galleryItemsPos);
+    console.log(widthArray);
+    console.log($(".gallery").width());
+
     $(".nav-able").click(function(event)
     {
         var target = $(this).attr("href");
@@ -12,4 +19,60 @@ $(document).ready(function()
 
         event.preventDefault();
     });   
+
+    $("body, html").click(function(event)
+    {
+        $(".gallery-item").removeClass("active-item");
+    })
+
+    $(".gallery-item").click(function(event)
+    {
+
+        $(".gallery-item").removeClass("active-item");
+        $(this).addClass("active-item");
+ 
+        console.log($(this).position());
+        var arrayIndex = $(this).index(".gallery-item");
+
+        var positionToMove = galleryItemsPos[arrayIndex] - 100;
+
+        // $(".gallery").animate(
+        // {
+        //     "left" : positionToMove + "px"
+        // }, 2000);
+
+        event.stopPropagation();
+        event.preventDefault();
+    });
 });
+
+function getPosOfGalleryItems()
+{
+    var array = [];
+    $(".gallery-item").each(function(index)
+    {
+        array.push($(this).position().left);
+    });
+
+    return array;
+}
+
+function getGalleryItemsWidth(posArray)
+{
+    var widthArray = [];
+
+    for (var i = 0; i < posArray.length; i++)
+    {
+        var width = posArray[i + 1] - posArray[i];
+        
+
+        if (i == posArray.length - 1)
+        {
+            width = $(".gallery").width() - posArray[i];
+        }
+
+        widthArray.push(width);
+    }
+
+    return widthArray;
+}
