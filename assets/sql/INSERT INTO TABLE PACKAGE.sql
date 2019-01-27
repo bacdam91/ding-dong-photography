89f306ff1be830831d553db349532aadd8bea7aa
bacdam91 <bacdam91@hotmail.com>
2019-01-28 00:38:41 +1100

    Added sql

---
+++ assets/sql/INSERT INTO TABLE PACKAGE.sql
@@ -0,0 +1,23 @@
+USE DINGDONGPHOTOGRAPHY;
+
+INSERT INTO PACKAGE (PackageName, PackageDesc, Cost) VALUES (
+	"Package #1",
+    'If you want something different from the norm then this is for YOU! As well as a full "Run and Gun" coverage of the event, I will also prepare an onsite studio-like set up which will capture you and your magical moment in another light! (Quite literally). As well stunning photos for family and group shots!',
+    450
+);
+
+INSERT INTO PACKAGE (PackageName, PackageDesc, Cost) VALUES (
+	"Package #2",
+    'This is my most popular package as the timeframe usually covers enough of the event while including most of what package 1 offers.',
+    350
+);
+
+INSERT INTO PACKAGE (PackageName, PackageDesc, Cost) VALUES (
+	"Package #3",
+    'Keeping it simple as possible, but not plain! Traditional "Run and Gun" style! Perfect for those wanting straight forward event photos and more budget friendly! If that\'s the case - this is it! ',
+    300
+);
+
+
+
+SELECT * FROM PACKAGE;
---
+++ assets/sql/INSERT INTO TABLE SERVICE.sql
@@ -0,0 +1,18 @@
+USE DINGDONGPHOTOGRAPHY;
+
+INSERT INTO SERVICE VALUES ("HOST", "Online gallery hosting", 0.00);
+INSERT INTO SERVICE VALUES ("ZIPF", "Zip file download", 0.00);
+INSERT INTO SERVICE VALUES ("PRNT", "Printable high resolution files", 0.00);
+INSERT INTO SERVICE VALUES ("PREP", "Early arrival for preparation", 0.00);
+INSERT INTO SERVICE VALUES ("EDIT", "Editing of all images", 0.00);
+INSERT INTO SERVICE VALUES ("TRVL", "Travel cost (unless 30 minutes away from Heidelberg)", 0.00);
+INSERT INTO SERVICE VALUES ("XTR1", "Extra 1 hour", 150.00);
+INSERT INTO SERVICE VALUES ("XTR2", "Extra 2 hours", 300.00);
+INSERT INTO SERVICE VALUES ("XTR3", "Extra 3 hours", 450.00);
+INSERT INTO SERVICE VALUES ("XTR4", "Extra 4 hours", 600.00);
+INSERT INTO SERVICE VALUES ("STUD", "Studio set up", 0.00);
+INSERT INTO SERVICE VALUES ("P4HR", "4 hours of photography coverage", 0.00);
+INSERT INTO SERVICE VALUES ("P3HR", "3 hours of photography coverage", 0.00);
+INSERT INTO SERVICE VALUES ("10PR", "10 free polaroids", 0.00);
+
+SELECT * FROM SERVICE;
